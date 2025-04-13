08b5d0bb93e471c1f8799c6abb118dd466bb2d31
CoderHung <161301206+CoderHung@users.noreply.github.com>
2025-04-13 23:56:36 +0700

    Update Vendor.sol

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,39 @@
 pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-    // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable {
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
     YourToken public yourToken;
+    uint256 public constant tokensPerEth = 100;
 
-    constructor(address tokenAddress) {
+    constructor(address tokenAddress) Ownable(msg.sender) {
         yourToken = YourToken(tokenAddress);
     }
 
-    // ToDo: create a payable buyTokens() function:
+    receive() external payable {}
 
-    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function buyTokens() external payable {
+        require(msg.value > 0, "Must send ETH to buy tokens");
+        uint256 amountOfTokens = msg.value * tokensPerEth;
+        require(yourToken.balanceOf(address(this)) >= amountOfTokens, "Vendor has insufficient tokens");
+        yourToken.transfer(msg.sender, amountOfTokens);
+        emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+    }
 
-    // ToDo: create a sellTokens(uint256 _amount) function:
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool success, ) = payable(owner()).call{value: balance}("");
+        require(success, "Withdrawal failed");
+    }
+
+    function sellTokens(uint256 _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(yourToken.balanceOf(msg.sender) >= _amount, "Insufficient token balance");
+        yourToken.transfer(address(this), _amount);
+        payable(msg.sender).transfer(_amount / tokensPerEth);
+    }
 }
